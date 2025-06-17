605d0e95a5a8f7ed9b2bfd9a79eb78dd38b9ba4f
Tayyab <mtayyabsohail8@gmail.com>
2025-06-17 10:52:43 +0500

    Update: Fixed form validation and user edit logic

--- src/views/UserDetail.vue
+++ src/views/UserDetail.vue
@@ -23,6 +23,13 @@ if (localUser) {
 </script>
 
 <template>
+    <RouterLink :to="'/'">
+
+        <button class="mb-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700  ml-5 cursor-pointer">
+            Back to Form
+        </button>
+
+    </RouterLink>
     <div v-if="user" class="max-w-xl mx-auto p-6 bg-white dark:bg-gray-800 rounded shadow">
         <h2 class="text-2xl font-bold"> Name : {{ user.name }}</h2>
         <p class="text-gray-600 dark:text-gray-300 mt-2"> Email : {{ user.email }}</p>
